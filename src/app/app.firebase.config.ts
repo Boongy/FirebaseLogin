d9a66d640a3f64e5ffaec31d7575d151b4d76c85
Mason Prochaska <Mason Prochaska>
2017-08-16 02:07:22 -0500

    Framework for firebase login system (email/password auth + profile tracking)

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyCSWu7_eR_jsiyY7bs52Aq6f23KoNKIdLo",
+  authDomain: "boongyandbuds.firebaseapp.com",
+  databaseURL: "https://boongyandbuds.firebaseio.com",
+  projectId: "boongyandbuds",
+  storageBucket: "boongyandbuds.appspot.com",
+  messagingSenderId: "427045839782"
+};
+
+export const FIREBASE_CONFIG = config;
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth} from "angularfire2/auth"
+import {AngularFireDatabase, FirebaseObjectObservable} from "angularfire2/database";
+import {Profile} from "../../models/profile";
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  profileData: FirebaseObjectObservable<Profile>
+
+  constructor(private afAuth: AngularFireAuth, private afDatabase: AngularFireDatabase, private toast: ToastController,
+              public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.afAuth.authState.subscribe(data => {
+      if(data && data.email && data.uid) {
+        this.toast.create({
+            message: 'Welcome to Boongy And Buds, ' + data.email,
+            duration:3000
+      }).present();
+
+
+        this.profileData = this.afDatabase.object('profile/'+data.uid);
+      }
+      else{
+        this.toast.create({
+          message: 'Could not find authentication details',
+          duration:3000
+        }).present();
+      }
+    });
 
   }
 
